5a325af04a7c010f11771a9a2d08b012ad929be5
shpairo711 <shapiro7@ajou.ac.kr>
2021-09-25 15:28:41 +0900

    fix: 스토리보드 main 찾고 있는 버그 해결

--- BoxOffice/Controller/SceneDelegate.swift
+++ BoxOffice/Controller/SceneDelegate.swift
@@ -15,11 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let mainViewController = ViewController()
-
-        window?.rootViewController = mainViewController
-        window?.makeKeyAndVisible()
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.backgroundColor = .white
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
